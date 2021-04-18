74ab128e7034a996b1c02205a5ca9edcbebec884
Lior Itach <44543472+lioritach@users.noreply.github.com>
2021-04-18 23:50:28 +0300

    Update RemoveVol.js

--- src/screens/admin/RemoveVol.js
+++ src/screens/admin/RemoveVol.js
@@ -24,73 +24,139 @@ const RemoveVol = ({ navigation }) => {
     return () => checkCity();
   }, []);
 
-
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <Card style={{ padding: 10, margin: 10, paddingTop: 2 }}>
-          <Text
-            style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
-          >
-            אנא בחר את הקטגוריה הרצויה
-          </Text>
-        </Card>
-        <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
-          <Button
-            onPress={() => {
-              navigation.navigate("adminRemoveVol", {
-                title: "ofakim_emergency",
-              });
-            }}
-            title="התנדבות בחירום"
-            color="#841584"
-          />
-        </Card>
-        <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
-          <Button
-            onPress={() => {
-              navigation.navigate("adminRemoveVol", {
-                title: "ofakim_covid19",
-              });
-            }}
-            title="התנדבויות בקורונה"
-            color="#841584"
-          />
-        </Card>
-        <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
-          <Button
-            onPress={() => {
-              navigation.navigate("adminRemoveVol", {
-                title: "ofakim_teens",
-              });
-            }}
-            title="התנדבויות לנוער"
-            color="#841584"
-          />
-        </Card>
-        <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
-          <Button
-            onPress={() => {
-              navigation.navigate("adminRemoveVol", {
-                title: "ofakim_religion",
-              });
-            }}
-            title="התנדבויות לפי מגדר"
-            color="#841584"
-          />
-        </Card>
-        <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
-          <Button
-            onPress={() => {
-              navigation.navigate("adminRemoveVol", {
-                title: "ofakim_routine",
-              });
-            }}
-            title="התנדבויות בשגרה"
-            color="#841584"
-          />
-        </Card>
-      </ScrollView>
+      {city == "אופקים" ? (
+        <ScrollView>
+          <Card style={{ padding: 10, margin: 10, paddingTop: 2 }}>
+            <Text
+              style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
+            >
+              אנא בחר את הקטגוריה הרצויה
+            </Text>
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_emergency",
+                });
+              }}
+              title="התנדבות בחירום"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_olds",
+                });
+              }}
+              title="התנדבות עם מבוגרים"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_covid19",
+                });
+              }}
+              title="התנדבויות בקורונה"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_teens",
+                });
+              }}
+              title="התנדבויות לנוער"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_religion",
+                });
+              }}
+              title="התנדבויות לפי מגדר"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "ofakim_routine",
+                });
+              }}
+              title="התנדבויות בשגרה"
+              color="#841584"
+            />
+          </Card>
+        </ScrollView>
+      ) : (
+        <ScrollView>
+          <Card style={{ padding: 10, margin: 10, paddingTop: 2 }}>
+            <Text
+              style={{ fontWeight: "bold", fontSize: 18, textAlign: "center" }}
+            >
+              אנא בחר את הקטגוריה הרצויה
+            </Text>
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "beersheva_emergency",
+                });
+              }}
+              title="התנדבות בחירום"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "beersheva_covid19",
+                });
+              }}
+              title="התנדבויות בקורונה"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "beersheva_teens",
+                });
+              }}
+              title="התנדבויות לנוער"
+              color="#841584"
+            />
+          </Card>
+          <Card style={{ padding: 10, margin: 20, paddingTop: 2 }}>
+            <Button
+              onPress={() => {
+                navigation.navigate("adminRemoveVol", {
+                  title: "beersheva_gmach",
+                });
+              }}
+              title="התנדבויות בגמחים"
+              color="#841584"
+            />
+          </Card>
+        </ScrollView>
+      )}
     </View>
   );
 };
